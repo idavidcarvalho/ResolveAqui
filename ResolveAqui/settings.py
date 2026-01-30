4da9536a9614205d44de310a18501cdd0a09796e
David Carvalho <davidcarvalho.dev@gmail.com>
2026-01-29 21:31:13 -0300

    feat(templates): adiciona diretorio para os templates

--- ResolveAqui/settings.py
+++ ResolveAqui/settings.py
@@ -107,3 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Optional: where `collectstatic` will collect files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
