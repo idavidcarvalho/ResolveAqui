93ce755b3bcb4ceada7de54d46c29bd2f76a7ed9
David Carvalho <davidcarvalho.dev@gmail.com>
2026-02-14 08:43:30 -0300

    feat(tickets): configura painel administrativo

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Ticket, Area, Problem
 
-# Register your models here.
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+	search_fields = ('name',)
+
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+	list_display = ('name', 'area')
+	list_filter = ('area',)
+	search_fields = ('name',)
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+	list_display = ('problem', 'created_by', 'district', 'status', 'created_at')
+	list_filter = ('district', 'status', 'created_at')
+	search_fields = ('problem__name', 'address', 'created_by__email')
